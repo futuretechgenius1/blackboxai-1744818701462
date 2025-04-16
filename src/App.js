f2c907ecd252982dfc7eb85013fc4a9390e6e026
futuretechgenius1 <manoharyadavit@gmail.com>
2025-04-16 21:21:51 +0530

    Add src/App.js 1744818711587

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RulesTable from './components/RulesTable';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Regulatory Rules Table</h1>
+      </header>
+      <RulesTable />
+    </div>
+  );
+}
+
+export default App;
